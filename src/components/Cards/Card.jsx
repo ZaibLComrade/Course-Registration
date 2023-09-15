14ecbae36f3541f96b5eec3df42ddad72dae9c52
ZaibLComrade <kmjahanzaib@gmail.com>
2023-09-15 15:12:40 +0600

    Styled cards

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,12 +1,33 @@
 import PropTypes from 'prop-types'
+import { FiDollarSign } from 'react-icons/fi'
+import { TfiBook } from 'react-icons/tfi'
 
 export default function Card({ content }) {
-	const { course_name, img } = content;
-	return <div className="bg-white border-2">
+	const bottomStyle = "flex items-center gap-3"
+	const { course_name, credit, img, description, price} = content;
+	const logoStyle = "text-2xl"
+	return <div className="flex flex-col justify-between p-4 bg-white rounded-lg">
+
 		<div>
-			<img src={ img } alt=""/>
+			<div className="mb-4 rounded-xl">
+				<img src={ img } className="object-cover w-full h-full" alt=""/>
+			</div>
+			<div className="mb-5">
+				<h1 className="mb-3 text-lg text-custom-black">{ course_name }</h1>
+				<p className="text-custom-black/60">{ description }</p>
+			</div>
 		</div>
-		<h1>{ course_name }</h1>
+		
+		<div className="flex justify-around">
+			<div className={ bottomStyle }>
+				<FiDollarSign className={ logoStyle }/>
+				<p className="text-custom-black/60">Price: { price }</p>
+			</div>
+			<div className={ bottomStyle }>
+				<TfiBook className={ logoStyle }/>
+				<p className="text-custom-black/60">Credit: { credit }hr</p>
+			</div>
+		</div>
 	</div>
 }
 
