325be9719936093028ae61ca280b3892c63f8437
ZaibLComrade <kmjahanzaib@gmail.com>
2023-09-15 12:22:02 +0600

    Fetched json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import './App.css'
-import Header from './components/Header'
+import { useEffect } from 'react';
+import './App.css';
+import Header from './components/Header';
 
 function App() {
+
+	// Loading API resources
+	useEffect(() => {
+		fetch("courseData.json")
+			.then(response => response.json())
+			.then(resource => console.log(resource))
+	}, [])
 
   return (
     <>
